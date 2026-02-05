2739e22ac145a817ea43f336fd24f5fb4c6dab7b
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2026-02-05 02:22:49 -0400

    keep working on blue

--- blue/src/blue.scm
+++ blue/src/blue.scm
@@ -13,5 +13,41 @@
   (eq? 'if (car expr)))
 
 ;; eval
-(define (b-eval expr env cont str)
-  (cond ((
+
+;; apply
+
+;; repl
+(define (blue level turn)
+  (lambda (ans)
+    (lambda (Mcont)
+      (write level) (write '-) (write turn) (display ": ")
+      (write ans)
+      (newline)
+      (write level) (write '-) (write (+ 1 turn)) (display " BLUE> ")
+      (let ((r (read)))
+	(if (eq? r 'switch)
+	    ((Mcont '(switch to graph)) (blue level turn))
+	    (((blue level (+ turn 1)) r) Mcont))))))
+
+(define (graph level turn)
+  (lambda (ans)
+    (lambda (Mcont)
+      (write level) (write '-) (write turn) (display ": ")
+      (write ans)
+      (newline)
+      (write level) (write '-) (write (+ 1 turn)) (display " GRAPH> ")
+      (let ((r (read)))
+	(if (eq? r 'switch)
+	    ((Mcont '(switch to blue)) (graph level turn))
+	    (((graph level (+ turn 1)) r) Mcont))))))
+
+;; initialize
+(define (init-repls)
+  (cons (blue 0 0)
+	(graph 0 0)))
+
+(define (main)
+  (let* ((repls (init-repls))
+	 (blue-repl (car repls))
+	 (graph-repl (cdr repls)))
+    ((blue-repl 'start) graph-repl)))
