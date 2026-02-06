c519826e59769a98bbecb7fd46993c3643696664
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2026-02-05 23:56:02 -0400

    focusing on the metacircular interpreter CPS

--- blue/src/blue.scm
+++ blue/src/blue.scm
@@ -2,35 +2,51 @@
 ;; Description: Main module
 (load "tensor.scm")
 
-;; expr
+;; predicates
 (define (constant? expr)
   (or (number? expr)
-      (tensor? expr)
       (boolean? expr)))
-
-(define (var? expr)
-  (symbol? expr))
 
 (define (if? expr)
   (eq? 'if (car expr)))
 
+;; accessors
+(define (if-pred expr) (cadr expr))
+
+(define (if-then expr) (caddr expr))
+
+(define (if-else expr)
+  (let ((melse (cdddr expr)))
+    (if (null? melse)
+	#f
+	(cadddr expr))))
+
 ;; eval
-(define (blue-eval expr)
-  (cond ((constant? expr) expr)
-	(else (error expr "not yet implemented"))))
+(define (blue-base-eval expr env cont str)
+  (cond ((constant? expr) (cont expr))
+	((if? expr) (eval-if expr env cont str))
+	(else (error expr "unknown construct"))))
+
+(define (eval-if expr env cont str)
+  (blue-base-eval (if-pred expr)
+		  env
+		  (lambda (p)
+		    (if p
+			(blue-base-eval (if-then expr) env cont str)
+			(blue-base-eval (if-else expr) env cont str)))
+		  str))
 
 ;; repl
-(define (blue level turn)
+(define (blue env str level turn)
   (lambda (ans)
     (lambda (Mcont)
       (write level) (write '-) (write turn) (display ": ")
       (write ans)
       (newline)
-      (write level) (write '-) (write (+ 1 turn)) (display " BLUE> ")
-      (let ((r (read)))
-	(if (eq? r 'switch)
-	    ((Mcont '(switch to graph)) (blue level turn))
-	    (((blue level (+ turn 1)) r) Mcont))))))
+      (write level) (write '-) (write (+ 1 turn)) (display " BLUE ]=> ")
+      (blue-base-eval (read) env (lambda (ans)
+				   (((blue env str level (+ turn 1)) ans) Mcont))
+		      str))))
 
 (define (graph level turn)
   (lambda (ans)
@@ -46,7 +62,7 @@
 
 ;; initialize
 (define (init-repls)
-  (cons (blue 0 0)
+  (cons (blue '() '() 0 0)
 	(graph 0 0)))
 
 (define (main)
